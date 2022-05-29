ea297cf957b7354a282b0f45a4ee18705431ff9d
tahalahij <t.ghasemi@emofid.com>
2022-05-29 23:18:10 +0430

    test: add unit tests category.service

--- src/modules/category/category.service.spec.ts
+++ src/modules/category/category.service.spec.ts
@@ -1,175 +1,137 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
 import { CategoryModule } from './category.module';
-import { Category } from './category.model';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { Messages } from '../shared/messages.constant';
-import { getConnection } from 'typeorm';
-import { CreateDto } from './category.dto';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { Category, CategoryDocument, CategorySchema } from './models/category.model';
+import { Model, Types } from 'mongoose';
+import { MESSAGES } from './constants/constants';
 
+const getMockId = () => new Types.ObjectId();
+const getMockCategory = (
+  override: Partial<Category> = {},
+): {
+  parent: (() => Types.ObjectId) | Types.ObjectId;
+  name: string;
+  discount: number;
+  _id: Types.ObjectId;
+} => {
+  return {
+    _id: getMockId(),
+    name: 'CATEGORY',
+    parent: getMockId(),
+    discount: 10,
+    ...override,
+  };
+};
 describe('Category Service Test', () => {
-  let categoryService: CategoryService;
+  describe('createCategory', () => {
+    let categoryService: CategoryService;
+    let categoryModel: Model<CategoryDocument>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          envFilePath: '.env',
-        }),
-        TypeOrmModule.forRootAsync({
-          useFactory: () => {
-            return {
-              type: 'postgres',
-              host: process.env.DB_HOST,
-              port: +process.env.DB_PORT,
-              username: process.env.DB_USER,
-              password: process.env.DB_PASS,
-              database: 'postgres',
-              entities: [Category, Category],
-              synchronize: true,
-              ssl: false,
-              keepConnectionAlive: true,
-            };
-          },
-        }),
-        CategoryModule,
-        CategoryModule,
-        TypeOrmModule.forFeature([Category, Category]),
-      ],
-      providers: [CategoryService],
-    }).compile();
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        imports: [
+          ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+          }),
+          MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
+          MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+              uri: configService.get<string>('MONGODB_URI'),
+            }),
+            inject: [ConfigService],
+          }),
+          CategoryModule,
+        ],
+        providers: [CategoryService],
+      }).compile();
 
-    categoryService = module.get<CategoryService>(CategoryService);
-  });
-
-  afterEach(async () => {
-    jest.clearAllMocks();
-    jest.restoreAllMocks();
-    const entities = getConnection().entityMetadatas;
-    for (const entity of entities) {
-      const repository = await getConnection().getRepository(entity.name);
-      await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
-    }
-  });
-
-  it('should be defined', () => {
-    expect(categoryService).toBeDefined();
-  });
-
-  it('should create new category', async () => {
-    const data: CreateDto = {
-      name: 'some name',
-      parent: null,
-      discount: 20,
-    };
-    const result = await categoryService.createCategory(data);
-    expect(result.name).toBe(data.name);
-    expect(result.discount).toBe(data.discount);
-    expect(result.parent).toBe(data.parent);
-  });
-
-  it('should throw notFound on create category with invalid parent', async () => {
-    try {
-      const data: CreateDto = {
-        name: 'some name',
-        parent: null,
-        discount: 20,
-      };
-      await categoryService.createCategory(data);
-    } catch (error) {
-      expect(error.message).toBe(Messages.NOT_FOUND);
-    }
-  });
-
-  it('should throw conflict on duplicate category', async () => {
-    try {
-      const data: CreateDto = {
-        name: 'some name',
-        parent: null,
-        discount: 20,
-      };
-      const data2: CreateDto = {
-        name: 'some name',
-        parent: null,
-        discount: 20,
-      };
-      await categoryService.createCategory(data);
-      await categoryService.createCategory(data2);
-    } catch (error) {
-      expect(error.message).toBe(Messages.ALREADY_EXISTS);
-    }
-  });
-
-  it('should get category list', async () => {
-    const data: CreateDto = {
-      name: 'some name',
-      parent: null,
-      discount: 20,
-    };
-    await categoryService.createCategory(data);
-    const result = await categoryService.getCategories();
-    expect(result.length).toBeGreaterThan(0);
-  });
-
-  it('should get category by id', async () => {
-    const data: CreateDto = {
-      name: 'some name',
-      parent: null,
-      discount: 20,
-    };
-    const created = await categoryService.createCategory(data);
-    const result = await categoryService.getCategoryById(created.id);
-    expect(result.id).toBe(created.id);
-  });
-
-  it('should throw notFound to get category by id', async () => {
-    try {
-      await categoryService.getCategoryById(-1);
-    } catch (error) {
-      expect(error.message).toBe(Messages.NOT_FOUND);
-    }
-  });
-
-  it('should update category by id', async () => {
-    const data: CreateDto = {
-      name: 'some name',
-      parent: null,
-      discount: 20,
-    };
-    const created = await categoryService.createCategory(data);
-    const updated = await categoryService.updateCategory(created.id, {
-      name: 'updated name',
-      parent: null,
-      discount: 10,
+      categoryService = module.get<CategoryService>(CategoryService);
+      categoryModel = module.get<Model<CategoryDocument>>(getModelToken('Category'));
     });
-    expect(updated.id).toBe(created.id);
-    expect(updated.parent).toBe(created.parent);
-    expect(updated.name).toBe('updated name');
-    expect(updated.discount).toBe(10);
-  });
 
-  it('should throw notFound update category by id', async () => {
-    try {
-      await categoryService.updateCategory(-1, {
-        name: 'some name',
-        parent: null,
-        discount: 20,
+    afterEach(async () => {
+      await categoryModel.remove({});
+      jest.restoreAllMocks();
+    });
+
+    it('should be defined', () => {
+      expect(categoryService).toBeDefined();
+    });
+    describe('createCategory', () => {
+      it('should be defined', () => {
+        expect(categoryService.createCategory).toBeDefined();
       });
-    } catch (error) {
-      expect(error.message).toBe(Messages.NOT_FOUND);
-    }
-  });
+      it('should create new category', async () => {
+        const name = 'ai';
+        const discount = 5;
+        const category = await categoryService.createCategory({
+          name,
+          discount,
+        });
+        expect(category).toHaveProperty('name', name);
+        expect(category).toHaveProperty('discount', discount);
+        expect(category.parent).toBeFalsy();
+      });
+    });
 
-  it('should delete category by id', async () => {
-    const data = {
-      name: 'some name',
-      parent: null,
-      discount: 20,
-    };
-    const created = await categoryService.createCategory(data);
-    const result = await categoryService.deleteCategory(created.id);
-    expect(result.affected).toBe(1);
+    describe('getCategoryById', () => {
+      it('should be defined', () => {
+        expect(categoryService.getCategoryById).toBeDefined();
+      });
+      it('should return category', async () => {
+        const name = 'ai';
+        const discount = 5;
+        const doc = await categoryModel.create(getMockCategory({ name, discount }));
+        const category = await categoryService.getCategoryById(doc._id);
+        expect(category).toHaveProperty('name', doc.name);
+        expect(category).toHaveProperty('discount', doc.discount);
+        expect(category).toHaveProperty('parent', doc.parent);
+      });
+    });
+
+    describe('getCategoryById', () => {
+      it('should be defined', () => {
+        expect(categoryService.getCategoryById).toBeDefined();
+      });
+      it('should return category', async () => {
+        const name = 'ai';
+        const discount = 5;
+        const doc = await categoryModel.create(getMockCategory({ name, discount }));
+        const category = await categoryService.getCategoryById(doc._id);
+        expect(category).toHaveProperty('name', doc.name);
+        expect(category).toHaveProperty('discount', doc.discount);
+        expect(category).toHaveProperty('parent', doc.parent);
+      });
+      it('should throw error if category not found', async () => {
+        await expect(categoryService.getCategoryById(getMockId())).rejects.toThrow(
+          MESSAGES.CATEGORY_NOT_FOUND,
+        );
+      });
+    });
+
+    describe('updateCategory', () => {
+      it('should be defined', () => {
+        expect(categoryService.updateCategory).toBeDefined();
+      });
+      it('should return updated category', async () => {
+        const name = 'ai';
+        const discount = 5;
+        const parent = getMockId();
+        const doc = await categoryModel.create(getMockCategory({}));
+        const category = await categoryService.updateCategory(doc._id, { discount, parent, name });
+        expect(category).toHaveProperty('name', name);
+        expect(category).toHaveProperty('discount', discount);
+        expect(category).toHaveProperty('parent', parent);
+      });
+      it('should throw error if category not found', async () => {
+        await expect(
+          categoryService.updateCategory(getMockId(), { name: 'new name' }),
+        ).rejects.toThrow(MESSAGES.CATEGORY_NOT_FOUND);
+      });
+    });
   });
 });
