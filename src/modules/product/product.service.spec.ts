117b1e51a05612f0cc5376c338a579df1eee8ed6
tahalahij <t.ghasemi@emofid.com>
2022-05-30 00:11:06 +0430

    test: unit tests for product.service

--- src/modules/product/product.service.spec.ts
+++ src/modules/product/product.service.spec.ts
@@ -1,161 +1,218 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
 import { ProductModule } from './product.module';
-import { Product } from './product.model';
-import { CategoryModule } from '../category/category.module';
-import { Category } from '../category/category.model';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { Messages } from '../shared/messages.constant';
-import { getConnection } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { Product, ProductDocument, ProductSchema } from './models/product.model';
+import { Model, Types } from 'mongoose';
+import { MESSAGES } from './constants/constants';
 
+const getMockId = () => new Types.ObjectId();
+const getMockProduct = (
+  override: Partial<Product> = {},
+): {
+  parent: (() => Types.ObjectId) | Types.ObjectId;
+  code: string;
+  name: string;
+  discount: number;
+  _id: Types.ObjectId;
+} => {
+  return {
+    name: 'NAME',
+    code: 'CODE',
+    _id: getMockId(),
+    parent: getMockId(),
+    discount: 10,
+    ...override,
+  };
+};
 describe('Product Service Test', () => {
   let productService: ProductService;
+  let productModel: Model<ProductDocument>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
           isGlobal: true,
           envFilePath: '.env',
         }),
-        TypeOrmModule.forRootAsync({
-          useFactory: () => {
-            return {
-              type: 'postgres',
-              host: process.env.DB_HOST,
-              port: +process.env.DB_PORT,
-              username: process.env.DB_USER,
-              password: process.env.DB_PASS,
-              database: 'postgres',
-              entities: [Product, Category],
-              synchronize: true,
-              ssl: false,
-              keepConnectionAlive: true,
-            };
-          },
+        MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGODB_URI'),
+          }),
+          inject: [ConfigService],
         }),
         ProductModule,
-        CategoryModule,
-        TypeOrmModule.forFeature([Product, Category]),
       ],
       providers: [ProductService],
     }).compile();
 
     productService = module.get<ProductService>(ProductService);
+    productModel = module.get<Model<ProductDocument>>(getModelToken('Product'));
   });
 
   afterEach(async () => {
-    jest.clearAllMocks();
+    await productModel.remove({});
     jest.restoreAllMocks();
-    const entities = getConnection().entityMetadatas;
-    for (const entity of entities) {
-      const repository = await getConnection().getRepository(entity.name);
-      await repository.query(
-        `TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`,
-      );
-    }
   });
-
   it('should be defined', () => {
     expect(productService).toBeDefined();
   });
-
-  it('should create new product', async () => {
-    const data = {
-      name: 'some name',
-      category: null,
-    };
-    const result = await productService.create(data);
-    expect(result.name).toBe(data.name);
-    expect(result.category).toBe(data.category);
-  });
-
-  it('should throw notFound on create product with invalid category', async () => {
-    try {
-      const data = {
-        name: 'some name other name',
-        category: -1,
-      };
-      await productService.create(data);
-    } catch (error) {
-      expect(error.message).toBe(Messages.NOT_FOUND);
-    }
-  });
-
-  it('should throw conflict on duplicate product', async () => {
-    try {
-      const data = {
-        name: 'some name',
-        category: null,
-      };
-      await productService.create(data);
-    } catch (error) {
-      expect(error.message).toBe(Messages.ALREADY_EXISTS);
-    }
-  });
-
-  it('should get product list', async () => {
-    const data = {
-      name: 'some name',
-      category: null,
-    };
-    await productService.create(data);
-    const result = await productService.list();
-    expect(result.length).toBeGreaterThan(0);
-  });
-
-  it('should get product by id', async () => {
-    const data = {
-      name: 'some name',
-      category: null,
-    };
-    const created = await productService.create(data);
-    const result = await productService.find(created.id);
-    expect(result.id).toBe(created.id);
-  });
-
-  it('should throw notFound to get product by id', async () => {
-    try {
-      await productService.find(-1);
-    } catch (error) {
-      expect(error.message).toBe(Messages.NOT_FOUND);
-    }
-  });
-
-  it('should update product by id', async () => {
-    const data = {
-      name: 'some name',
-      category: null,
-    };
-    const created = await productService.create(data);
-    const updated = await productService.update(created.id, {
-      name: 'updated name',
-      category: null,
+  describe('createProduct', () => {
+    it('should be defined', () => {
+      expect(productService.createProduct).toBeDefined();
     });
-    expect(updated.id).toBe(created.id);
-    expect(updated.category).toBe(created.category);
-    expect(updated.name).toBe('updated name');
-  });
-
-  it('should throw notFound update product by id', async () => {
-    try {
-      await productService.update(-1, {
-        name: 'updated name',
-        category: null,
+    it('should create new product', async () => {
+      const code = 'product code';
+      const name = 'product name';
+      const parent = getMockId();
+      const discount = 5;
+      const product = await productService.createProduct({
+        code,
+        name,
+        parent,
+        discount,
       });
-    } catch (error) {
-      expect(error.message).toBe(Messages.NOT_FOUND);
-    }
+      expect(product).toHaveProperty('name', name);
+      expect(product).toHaveProperty('discount', discount);
+      expect(product).toHaveProperty('parent', parent);
+      expect(product).toHaveProperty('code', code);
+    });
+    it('should throw error of product with this code exists', async () => {
+      const code = 'product code';
+      const name = 'product name';
+      const parent = getMockId();
+      const discount = 5;
+      await productService.createProduct({
+        code,
+        name,
+        parent,
+        discount,
+      });
+      await expect(
+        productService.createProduct({
+          code,
+          name,
+          parent,
+          discount,
+        }),
+      ).rejects.toThrow(MESSAGES.PRODUCT_DUPLICATE_CODE);
+    });
   });
 
-  it('should delete product by id', async () => {
-    const data = {
-      name: 'some name',
-      category: null,
-    };
-    const created = await productService.create(data);
-    const result = await productService.remove(created.id);
-    expect(result.affected).toBe(1);
+  describe('getProductById', () => {
+    it('should be defined', () => {
+      expect(productService.getProductById).toBeDefined();
+    });
+    it('should return product', async () => {
+      const code = 'product code';
+      const name = 'product name';
+      const discount = 5;
+      const doc = await productModel.create(getMockProduct({ name, discount, code }));
+      const product = await productService.getProductById(doc._id);
+      expect(product).toHaveProperty('name', doc.name);
+      expect(product).toHaveProperty('discount', doc.discount);
+      expect(product).toHaveProperty('code', doc.code);
+      expect(product).toHaveProperty('parent', doc.parent);
+    });
+  });
+
+  describe('getProductById', () => {
+    it('should be defined', () => {
+      expect(productService.getProductById).toBeDefined();
+    });
+    it('should return product', async () => {
+      const code = 'product code';
+      const parent = getMockId();
+      const name = 'product name';
+      const discount = 5;
+      const doc = await productModel.create(getMockProduct({ name, discount, parent, code }));
+      const product = await productService.getProductById(doc._id);
+      expect(product).toHaveProperty('name', doc.name);
+      expect(product).toHaveProperty('discount', doc.discount);
+      expect(product).toHaveProperty('code', doc.code);
+      expect(product).toHaveProperty('parent', doc.parent);
+    });
+    it('should throw error if product not found', async () => {
+      await expect(productService.getProductById(getMockId())).rejects.toThrow(
+        MESSAGES.PRODUCT_NOT_FOUND,
+      );
+    });
+  });
+
+  describe('updateProduct', () => {
+    it('should be defined', () => {
+      expect(productService.updateProduct).toBeDefined();
+    });
+    it('should return updated product', async () => {
+      const code = 'product code';
+      const name = 'product name';
+      const discount = 5;
+      const parent = getMockId();
+      const doc = await productModel.create(getMockProduct({}));
+      const product = await productService.updateProduct(doc._id, {
+        discount,
+        parent,
+        name,
+        code,
+      });
+      expect(product).toHaveProperty('name', name);
+      expect(product).toHaveProperty('code', code);
+      expect(product).toHaveProperty('discount', discount);
+      expect(product).toHaveProperty('parent', parent);
+    });
+    it('should throw error of product with this code exists', async () => {
+      const code = 'product code';
+      const name = 'product name';
+      const parent = getMockId();
+      const discount = 5;
+      const product = await productModel.create({
+        code,
+        name,
+        parent,
+        discount,
+      });
+      await expect(
+        productService.updateProduct(product._id, {
+          code,
+        }),
+      ).rejects.toThrow(MESSAGES.PRODUCT_DUPLICATE_CODE);
+    });
+    it('should throw error if product not found', async () => {
+      await expect(productService.updateProduct(getMockId(), { code: 'new code' })).rejects.toThrow(
+        MESSAGES.PRODUCT_NOT_FOUND,
+      );
+    });
+  });
+
+  describe('getDiscount', () => {
+    it('should be defined', () => {
+      expect(productService.getDiscount).toBeDefined();
+    });
+    it('should return updated product', async () => {
+      const code = 'product code';
+      const name = 'product name';
+      const discount = 5;
+      const parent = getMockId();
+      const doc = await productModel.create(getMockProduct({}));
+      const product = await productService.updateProduct(doc._id, {
+        discount,
+        parent,
+        name,
+        code,
+      });
+      expect(product).toHaveProperty('name', name);
+      expect(product).toHaveProperty('code', code);
+      expect(product).toHaveProperty('discount', discount);
+      expect(product).toHaveProperty('parent', parent);
+    });
+    it('should throw error if product not found', async () => {
+      await expect(productService.updateProduct(getMockId(), { code: 'new code' })).rejects.toThrow(
+        MESSAGES.PRODUCT_NOT_FOUND,
+      );
+    });
   });
 });
