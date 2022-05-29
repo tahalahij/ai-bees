a25e250ba27b0a62b58c20cd5d4580c54fa305b3
tahalahij <t.ghasemi@emofid.com>
2022-05-29 20:47:16 +0430

    feat: add winston logger and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as winston from 'winston';
+import { WinstonModule } from 'nest-winston';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  if (!JSON.parse(configService.get('USE_NEST_LOGGER'))) {
+    app.useLogger(
+      WinstonModule.createLogger({
+        level: 'info',
+        format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        defaultMeta: { service: 'ai-bees' },
+        exitOnError: false,
+        transports: [
+          new winston.transports.Console(),
+          new winston.transports.File({
+            filename: 'logs/logs.log',
+          }),
+        ],
+      }),
+    );
+  }
+
+  const config = new DocumentBuilder()
+    .setTitle('AI Bees')
+    .setDescription('The AI Bees API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.setGlobalPrefix('api/v1');
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
